c14543cf400812739b321a3a018717c688d09c19
anton <anton.lantsov@gmail.com>
2025-07-31 18:58:49 +0200

    fix version

--- build_deb.sh
+++ build_deb.sh
@@ -5,7 +5,7 @@ echo "Building Debian package for voice-input..."
 # Use -d flag to override build dependencies check
 dpkg-buildpackage -us -uc -b -d
 
-if [ -f ../voice-input_0.1.1-1_*.deb ]; then
+if [ -f ../voice-input_0.1.2-1_*.deb ]; then
     echo "Debian package built successfully!"
     echo "You can install it with: sudo dpkg -i ../voice-input_0.1.2-1_*.deb"
     echo "And install any missing dependencies with: sudo apt-get install -f"
