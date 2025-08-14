dc300e8be42fedf103835839f07d19cd34e8192c
anton <anton.lantsov@gmail.com>
2025-08-14 19:49:31 +0200

    release 0.1.4

--- build_deb.sh
+++ build_deb.sh
@@ -5,9 +5,9 @@ echo "Building Debian package for voice-input..."
 # Use -d flag to override build dependencies check
 dpkg-buildpackage -us -uc -b -d
 
-if ls ../voice-input_0.1.3-1_*.deb >/dev/null 2>&1; then
+if ls ../voice-input_0.1.4-1_*.deb >/dev/null 2>&1; then
     echo "Debian package built successfully!"
-    echo "You can install it with: sudo dpkg -i ../voice-input_0.1.3-1_*.deb"
+    echo "You can install it with: sudo dpkg -i ../voice-input_0.1.4-1_*.deb"
     echo "If apt suggests installing NVIDIA compute package, you can accept it; otherwise the app will run on CPU."
     echo "To fix any missing dependencies: sudo apt-get -f install"
 else
